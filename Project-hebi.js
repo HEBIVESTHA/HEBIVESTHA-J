51f6e4d7433fb0abf8ab417e3fe278cad104c18b
HEBIVESTHA <83123328+HEBIVESTHA@users.noreply.github.com>
2021-05-18 20:17:46 +0530

    Add files via upload

---
+++ Project-hebi.js
@@ -0,0 +1,17 @@
+﻿const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/",function(req,res)
+{
+	res.sendFile(__dirname+"Project.html");
+	
+});
+app.post("/",function(req,res)
+{
+res.send("Thanks for posting");
+});
+app.listen(3000, function()
+{
+console.log("Server has started on port 3000");
+});
